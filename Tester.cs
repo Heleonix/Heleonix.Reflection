ff549e3872b4d03d2a4696dde19a4b5b77751c84
Hennadii Lutsyshyn <hennadii.lutsyshyn@gmail.com>
2024-05-03 00:46:39 +0300

    Revert "Remove the temp testing code"
    This reverts commit 1656118bbc5ee0fd0da37c9a7819695d6d80943e.

---
+++ Tester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Tester
+{
+    static void Main(string[] args)
+    {
+        string[] arrayTest = new string[] { "One", "Two" }
+
+        Collection<string> collectionTest = new Collection<string>() { "One", "Two"}
+
+        Dictionary<uint, string> dictionaryTest = new Dictionary<uint, string>();
+        dictionaryTest.Add(1, "One");
+        dictionaryTest.Add(2, "Two");
+
+        Reflector.Get(arrayTest, null, "[1]", out int value);
+        Console.WriteLine(value);
+    }
+}
